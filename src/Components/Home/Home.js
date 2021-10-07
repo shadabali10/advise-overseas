ff3c32445755a4f4ff10df5f8ded4c638e0d56c7
Rahul Kumar <vhrahulkumar@gmail.com>
2021-10-07 11:15:53 +0530

    edit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,14 +14,14 @@ const Home = () => {
     <>
       <Hero slides={SliderData} />
 
-      <section class="text-gray-600 body-font px-20">
+      <section class="text-gray-600 body-font lg:px-20 sm:px-10">
         <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
               Study Abroad
               <br class="hidden lg:inline-block" />
             </h1>
-            <p class="mb-8 leading-relaxed">
+            <p class="mb-8 leading-relaxed ">
               Study Abroad Programs give you the opportunity to experience the
               world as your classroom. Rather than picking things up from books
               and the Internet, navigating a new landscape may open your eyes to
